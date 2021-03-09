1fe06563fc40aa05bbe7b5163d32cc24d4b0e2c7
smeerws <hello@smeerws.at>
2021-03-09 10:04:56 +0100

    init ref prj for scenemanagement

---
+++ Assets/MyGame/Scripts/SceneLoaderWithParameter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderWithParameter : MonoBehaviour
+{
+    public void LoadNextSceneWithParamString(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextSceneWithBuildIndex(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
